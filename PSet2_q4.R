f794608a9cadf0326d5125475a32821df29af19a
lcfb8 <lydia.brosnahan@gmail.com>
2026-03-12 17:28:23 -0400

    updated for two table types

--- PSet2_q4.R
+++ PSet2_q4.R
@@ -1,6 +1,6 @@
 
 # Clean our raw data to get all test scores
-# 
+#
 # Script by Luke Miratrix
 
 library( tidyverse )
@@ -25,17 +25,21 @@ html_table( test_page_tables[[4]], fill = TRUE)
 
 all_units <- function(file_name){
   page = read_html( pages$file_name[[1]] )
-  
+
   page_tables = html_nodes( page, "table" )
-  
+
   html_table( page_tables[[4]], fill = TRUE)
 }
 
 
-all_tables <- map(pages$file_name[1:16], all_units) #asked chatgpt to help me with this, I wasn't using map() 
+all_tables <- map(pages$file_name[1:32], all_units)
+#asked chatgpt to help me with this, I wasn't using map()
+
+all_tables
 
 
-# Should we do the cleaning in this script? What else should we do here? Unnest the tables?
+# Should we do the cleaning in this script?
+# What else should we do here? Unnest the tables?
 
 
 
