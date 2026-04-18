23ff6dfc08e168aad9dff2b50296eedf2c88fac6
lcfb8 <lydia.brosnahan@gmail.com>
2026-04-17 21:13:47 -0400

    ridge, lasso, electric net models

---
+++ Pset3_predict_bullying/Ridge_lasso_elasticnet.R
@@ -0,0 +1,176 @@
+library(tidyverse)
+library(skimr)
+library(caret)     
+library(glmnet) 
+library(gridExtra)
+
+## Based on Lab 11
+
+set.seed(80107)
+
+cleaned_data = read.csv("cleaned_data.csv")
+
+#remove student_id
+cleaned_data <- cleaned_data[,-1]
+
+dim(cleaned_data)
+
+# Create a data partition such that 80% of the data is used for training and 
+# 20% for testing
+
+trainIndex <- createDataPartition(cleaned_data$bully,
+                                  p = .8, # the share of data in the training set
+                                  list = FALSE # puts our output in matrix form
+)
+
+cleaned_data <- cleaned_data %>% 
+  mutate(temp_id = 1:nrow(cleaned_data))
+
+# Use those indices to split the data into training and test sets
+train <- cleaned_data %>% 
+  filter(temp_id %in% trainIndex) %>% 
+  select(-temp_id)
+
+# Assign everything else to the test dataset 
+test <- cleaned_data %>% 
+  filter(!(temp_id %in% trainIndex)) %>% 
+  select(-temp_id)
+
+# Check how many rows are in each set
+nrow(train)
+nrow(test)
+
+# Convert predictors into a model matrix for glmnet
+# model.matrix() automatically creates dummy variables for categorical predictors
+# math ~ . means: predict math using all other variables in the dataset
+# [,-1] removes the intercept column because glmnet handles the intercept internally
+x_train_unscaled <- model.matrix(bully ~ ., train)[, -1]
+y_train <- train$bully
+
+x_test_unscaled <- model.matrix(bully ~ ., test)[, -1]
+y_test <- test$bully
+
+# Standardize the predictors in the training set
+# After scaling, each predictor is centered around 0 and has a standard deviation of about 1
+# This matters because regularization penalizes coefficient size
+# If predictors are on very different scales, the penalty will affect them unevenly
+x_train <- scale(x_train_unscaled)
+
+# Standardize the test set using the SAME means and standard deviations from the training set
+# attr(x_train, "scaled:center") stores the column means from the training data
+# attr(x_train, "scaled:scale") stores the column standard deviations from the training data
+# We reuse those values so the test set is transformed in the same way as the training set
+x_test <- scale(
+  x_test_unscaled,
+  center = attr(x_train, "scaled:center"),
+  scale = attr(x_train, "scaled:scale")
+)
+
+###### now we'll test ridge, lasso, and elastic net all in one
+
+train_control <- trainControl(
+  method = "cv",
+  number = 10,
+  verboseIter = FALSE 
+)
+
+# Define a set of alpha values to try. We'll include 0 and 1 to test ridge and
+# lasso along with the other values for elastic net
+
+alpha_grid <- seq(0, 1, by = 0.1) 
+lambda_grid <- 10 ^ seq(10, -2, length = 100)
+
+set.seed(80107)
+
+rle_model <- train(
+  x = x_train,
+  y = y_train,
+  method = "glmnet",
+  trControl = train_control,
+  tuneGrid = expand.grid(alpha = alpha_grid, lambda = lambda_grid),
+  metric = "RMSE"
+)
+
+
+# Plot RMSE across tuning values
+plot(rle_model, xTrans = log)
+
+# View the best alpha
+best_alpha <- rle_model$bestTune$alpha
+best_alpha
+
+# View what caret says is the best lambda (lowest RMSE)
+best_lambda_caret <- rle_model$bestTune$lambda
+best_lambda_caret
+
+# ChatGPT helped me understand that caret and glmnet packages have different
+# ways of finding the best lambda. Let's look at the glmnet version
+cv.out <- cv.glmnet(x_train, y_train, alpha=best_alpha)
+plot(cv.out)
+
+cv.out
+
+# lambda min has 69 nonzero coefs and lambda 1se has 29 nonzero coefs
+# lambda min of 0.01 matches caret, but lambda 1se of 0.08 is different
+
+# let's store this best lambda (1se) as well
+best_lambda_glmnet <- cv.out$lambda.1se
+log(best_lambda_glmnet)
+
+# Extract coefficients at the best lambda
+coefs <- coef(rle_model$finalModel, alpha = best_alpha, s = best_lambda)
+
+coefs <- coefs[which(coefs != 0),]
+
+coefs_df <- data.frame(
+  Variable = rownames(as.matrix(coefs)),
+  Coefficient = as.numeric(coefs)
+)
+
+# Inspect largest coefficients
+coefs_df %>%
+  arrange(desc(abs(Coefficient))) %>% 
+  head(10)
+
+# Now lets run our best model on the testing data
+# First let's try the caret version (best lambda minimizes RMSE)
+rle.pred.caret = predict(rle_model, newdata=x_test)
+
+# How variable are our predictions?
+summary( rle.pred.caret )
+sd( rle.pred.caret )
+
+# The RMSE
+sqrt( mean((rle.pred-y_test)^2) )
+
+# Now let's repeat with the glmnet version using lambda 1se
+rle.pred.glmnet = predict(rle_model$finalModel, 
+                          s = best_lambda_glmnet, newx=x_test)
+
+# How variable are our predictions?
+summary( rle.pred.glmnet )
+sd( rle.pred.glmnet )
+
+# The RMSE
+sqrt( mean((rle.pred.glmnet-y_test)^2) )
+
+# both of these RMSEs are quite low
+
+# Let's look at a plot
+
+#put both sets of predictions into the test df
+test_plot <- test %>% 
+  mutate(y_pred_caret = rle.pred.caret, y_pred_glmnet = rle.pred.glmnet)
+
+test_plot <- test_plot %>% 
+  pivot_longer(cols = c(y_pred_glmnet,y_pred_caret, bully), names_to = "model", 
+               values_to = "y")
+
+test_plot %>% 
+  ggplot(aes(age,y, col = model))+
+  geom_point()+
+  geom_jitter()
+
+test_plot %>% 
+  ggplot(aes(model,y)) +
+  geom_boxplot()
