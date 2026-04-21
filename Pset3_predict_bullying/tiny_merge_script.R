7d81dc06e068275134fb4189937f06beb7b6cf90
lcfb8 <lydia.brosnahan@gmail.com>
2026-04-20 20:14:00 -0400

    final prediction csv

---
+++ Pset3_predict_bullying/tiny_merge_script.R
@@ -0,0 +1,23 @@
+library( tidyverse )
+
+
+rf_class <- read_csv("rf_class_predictions.csv")
+
+rf_ranger <- read_csv("final_rangder_random_forest_prediction.csv")
+
+log_step_risk <- read_csv("log_step_risk_predictions.csv")
+
+rf_class = rf_class %>% 
+  select(student_id,predicted_bully_high)
+
+dim(rf_class)
+dim(rf_ranger)
+dim(log_step_risk)
+
+#merge (by student_id) rf_class, rf_ranger, and log_step_risk into one dataset 
+#called student_test_data.csv 
+student_test_data <- rf_class %>% 
+  left_join(rf_ranger, by = "student_id") %>% 
+  left_join(log_step_risk, by = "student_id")
+
+dim(student_test_data)
