385bf4d0d4cd60f1382f4647ac6959c6d2c88618
lcfb8 <lydia.brosnahan@gmail.com>
2026-04-20 20:14:23 -0400

    tweak

--- Pset3_predict_bullying/tiny_merge_script.R
+++ Pset3_predict_bullying/tiny_merge_script.R
@@ -16,8 +16,16 @@ dim(log_step_risk)
 
 #merge (by student_id) rf_class, rf_ranger, and log_step_risk into one dataset 
 #called student_test_data.csv 
-student_test_data <- rf_class %>% 
+lambda_student_predictions <- rf_class %>% 
   left_join(rf_ranger, by = "student_id") %>% 
   left_join(log_step_risk, by = "student_id")
 
-dim(student_test_data)
+dim(lambda_student_predictions)
+
+write.csv(lambda_student_predictions, "lambda_student_predictions.csv", 
+          row.names = FALSE)
+
+source( "check_predictions_function.R" )
+check_prediction_file_format( "lambda_student_predictions.csv" )
+
+
