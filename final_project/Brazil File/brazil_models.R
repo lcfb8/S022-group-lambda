cfbbef36e5ad46d4e1b2a329c1579072bfa09992
lcfb8 <lydia.brosnahan@gmail.com>
2026-04-30 13:11:57 -0400

    I don't know

--- final_project/Brazil File/brazil_models.R
+++ final_project/Brazil File/brazil_models.R
@@ -4,21 +4,17 @@ library( broom )
 brazil_ed <- read_csv("brazil_ed_recoded.csv")
 alt_1996 <- read_csv("brazil_1996_alt.csv")
 brazil_edprop <- read_csv("brazil_ed_proportions.csv")
-brazil_all <- read_csv("brazil_all.csv")
-
-#let's make an inverse variable for unemployment
-
-brazil_all = brazil_all %>% 
-  mutate(employ = 100-unemploy)
+brazil_all <-read_csv("brazil_all.csv")
 
 
-M_loess <- loess(total_conc ~ employ + gdp, data = brazil_all, span = 0.2)
 
-#fix the below code 
+M_loess <- loess(total_conc ~ unemploy + gdp, data = brazil_all, span = 0.2)
 
-ggplot(brazil_all, aes(unemploy, total_conc, group = area, color = area))+
+head(brazil_all)
+
+
+brazil_all %>% 
+  mutate(area = as.factor(area)) %>% 
+  ggplot(aes(unemploy,total_conc,group = area, color = area))+
   geom_point()+
-  geom_smooth(method = "loess", span = 0.5, se = FALSE)
-
-#what's the r squared for M_loess
-summary(M_loess)$r.squared
+  geom_smooth()
